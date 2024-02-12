2f233bf89dad8bccce9d047f61a6ae2e538e1382
thomas pasquier ordi imo <lanouraf@gmail.com>
2024-02-12 10:37:36 +0100

    kk

---
+++ batchnorm.py
@@ -0,0 +1,15 @@
+pip -install pandas
+
+import os
+
+import streamlit as st
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.metrics import mean_absolute_error
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.linear_model import LinearRegression
+from torchvision.datasets import MNIST
+from torchvision.transforms import ToTensor
