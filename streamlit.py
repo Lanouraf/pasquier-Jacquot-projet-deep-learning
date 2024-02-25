ea0e3729c50634faaab4f4c214c7bf859a886ef4
Maud Jacquot <maud@jacquot.pro>
2024-02-25 19:33:00 +0100

    test.homemade.layernorm.v4

--- streamlit.py
+++ streamlit.py
@@ -4,11 +4,7 @@ import os
 import streamlit as st
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
-import zipfile
-import io
-import requests
-import dl
+import matplotlib.pyplot as pl
 
 def main():
     """The main function of the app.
