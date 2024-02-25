f0f6eeae66b2a8821570bc42779250b1e19f36b2
Maud Jacquot <maud@jacquot.pro>
2024-02-25 19:05:59 +0100

    charg.données.v6

--- streamlit.py
+++ streamlit.py
@@ -9,16 +9,6 @@ import matplotlib.pyplot as plt
 import zipfile
 import io
 import requests
-
-from sklearn.metrics import mean_absolute_error
-from sklearn.model_selection import train_test_split
-from sklearn.tree import DecisionTreeRegressor
-from sklearn.ensemble import RandomForestRegressor
-from sklearn.linear_model import LinearRegression
-from torchvision.datasets import MNIST
-from torchvision.transforms import ToTensor
-
-from viz import mnist_like_viz, training_curves
 import dl
 
 def main():
