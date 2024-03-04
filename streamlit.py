aa5788db2ad40ed163e623beae7b01bbe1cdd9b0
thomas pasquier ordi imo <lanouraf@gmail.com>
2024-03-04 10:54:14 +0100

    ajout de apple apple_quality

--- streamlit.py
+++ streamlit.py
@@ -74,6 +74,27 @@ def get_data():
     home_data = pd.read_csv(directory)
     return home_data
 
+
+
+@st.cache
+def get_data2():
+    """Loads the Apple quality data from Kaggle.
+
+    Returns
+    -------
+    df: pd.DataFrame
+        apple quality dataset to do classification tasks.
+
+    Notes
+    -----
+    This is the dataset dowloaded from
+        "https://www.kaggle.com/datasets/nelgiriyewithana/apple-quality"
+
+    """
+    directory = "apple_quality.csv"
+    home_data = pd.read_csv(directory)
+    return home_data
+
 def homemade_layernorm(home_data):
 
     st.text(
